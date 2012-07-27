57427507ca51b93b12eb4e06b7b0f945b504ab5b
Dennis Lipovsky <den@denlipovsky.com>
2012-07-26 21:23:57 -0400

    handling jsonp if the client requests it

--- server/server.js
+++ server/server.js
@@ -44,10 +44,19 @@ app.use(express.bodyParser());
 db.open(function(err, client) { if (err) {throw err;} });
 gameDb = new gamedb.GameDb(db);
 
+makeJsonp = function(jsonp, body) {
+    if(jsonp) {
+        return jsonp + "(" + body + ")";
+    }
+    else {
+        return body;
+    }
+};
 // Routes
 app.post('/game/init/:ailevel', function(req, res) {
     var ai = parseInt(req.params.ailevel);
     var nickname = req.body.nickname || 'anonymous';
+    var jsonp = req.body.jsonp;
 
     if (isNaN(ai) || ai < 1 || ai > 6) {
         res.end(JSON.stringify({error: 'ai level must be between 1 and 6, inclusive, with 1 being the easiest and 6 being the hardest'}));
@@ -58,7 +67,7 @@ app.post('/game/init/:ailevel', function(req, res) {
 	gameDb.init(
 	    theGame, 
 	    function(game){
-		res.end(JSON.stringify(theGame));
+            res.end(makeJsonp(jsonp, JSON.stringify(theGame)));
 	    }
 	);
     }
@@ -76,6 +85,7 @@ function findGame(gameId, callback) {
 app.post('/game/move/:gameId', function(req, res) {
     var gameId = req.params.gameId;
     var move = req.body.move;
+    var jsonp = req.body.jsonp;
     findGame(gameId, function(gameSpec) {
         gameSpec = game.deserialize(gameSpec);
         gameSpec.move(move); // make the player's move
@@ -83,7 +93,7 @@ app.post('/game/move/:gameId', function(req, res) {
         var callback = function(move) {
             gameSpec.move(move); // make the AI move
             gameDb.update(gameId, gameSpec, function(game) {
-                res.end(JSON.stringify(game));
+                res.end(makeJsonp(jsonp, JSON.stringify(game)));
             });
         };
         var ai = new computerplayer.ComputerPlayer(aiSpec.url, game.turn, callback);
