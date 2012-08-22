57b2c64b81d12312f75a78d61f83a3c000cbfdab
Dennis Lipovsky <den@denlipovsky.com>
2012-08-22 19:58:47 -0400

    missing commas, someone didn't run unit tests...

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ var PORT = 3000;
 
 var ais = {
     1: {url:'http://localhost:3001/ai/random'},
-    2: {url:'http://localhost:3001/ai/twostep'}
+    2: {url:'http://localhost:3001/ai/twostep'},
     3: {url:'http://localhost:3001/ai/random'},
-    4: {url:'http://localhost:3001/ai/twostep'}
+    4: {url:'http://localhost:3001/ai/twostep'},
     5: {url:'http://localhost:3001/ai/random'},
     6: {url:'http://localhost:3001/ai/twostep'}
 }
