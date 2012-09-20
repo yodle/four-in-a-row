4f1905b0897fbf3d73c81123b1807d908e673be0
Mark Drago <markdrago@gmail.com>
2012-09-20 07:55:22 -0400

    add some tests around the handling of httpresponses

--- clients/go/src/c4/game/restclient_test.go
+++ clients/go/src/c4/game/restclient_test.go
@@ -1,6 +1,9 @@
 package game
 
 import (
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -60,4 +63,44 @@ func TestGetApiTargetLeavesOneSlashBetweenBaseUrlAndMethodEvenIfTwoWereGiven(t *
 	if target != expected {
 		t.Errorf("getApiTarget left 2 slashes between base url & method.  Expected '%s', Got '%s'", expected, target)
 	}
+}
+
+func TestGetStringFromReaderWorks(t *testing.T) {
+	str := "The brown fox ate the lazy dog"
+	reader := strings.NewReader(str)
+	result := getStringFromReader(reader)
+	if result != str {
+		t.Errorf("getStringFromReader did not get the correct string.  Expected '%s', Got '%s'", str, result)
+	}
+}
+
+func TestHandleHttpResponseReturnsHttpBody(t *testing.T) {
+	resp := new(http.Response)
+	var err error
+
+	str := "The brown fox ate the lazy dog"
+	resp.Body = NewMockResponseBody(str)
+
+	result := handleHttpResponse(resp, err)
+
+	if result != str {
+		t.Errorf("handleHttpResponse did not return proper string from Body.  Expected '%s', Got '%s'", str, result)
+	}
+}
+
+type MockResponseBody struct {
+	reader io.Reader
+}
+
+func NewMockResponseBody(str string) *MockResponseBody {
+	reader := strings.NewReader(str)
+	return &MockResponseBody{reader}
+}
+
+func (mock *MockResponseBody) Read(p []byte) (n int, err error) {
+	return mock.reader.Read(p)
+}
+
+func (mock *MockResponseBody) Close() error {
+	return nil
 }
