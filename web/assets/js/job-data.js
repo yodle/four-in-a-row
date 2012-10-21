244750fc0f4f6ce8a858028655e17bbf4566aa53
Dennis Lipovsky <dlipovsky@yodle.com>
2012-10-21 15:46:02 -0400

    pull job listings to main page

--- web/assets/js/job-data.js
+++ web/assets/js/job-data.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
             var category = $(this).find("category").text();
             if(category === "Technology") {
                 var title = $(this).find("title").text();
-                var detailUrl = $(this).find("detail-url").text();
+                var detailUrl = $(this).find("detail-url").html().replace("<!--[CDATA[", "").replace("]]-->", "");
                 jobListings.append("<li><a href="+detailUrl+">"+title+"</a></li>")
             }
         });
